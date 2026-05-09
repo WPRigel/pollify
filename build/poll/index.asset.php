afa2c7d833e908fab5d44bcd21d3fd5751a954e7
Sabbir Ahmed <sabbir.081070@gmail.com>
2026-05-09 18:28:35 +0600

    Build all assets and tested

--- build/poll/index.asset.php
+++ build/poll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b81816550a237cf0f431');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9e300ee6f4b216691f54');
--- build/poll/view.asset.php
+++ build/poll/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => 'b9142f5a6a01851f7f5d');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => '3ff73ba3961027ee96ac');
